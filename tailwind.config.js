fc53a630d755475154c09e06ee55754ba9ac9505
IgorShumsky <Tracho2013@gmail.com>
2026-07-03 16:39:26 +0300

    add toggle them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector", // В старых версиях Tailwind это могло называться 'class'
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
